00e738769c50f3740facb8d6fb60504e73e57d3c
末日沙兔 <2657487207@qq.com>
2021-12-17 21:07:23 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -12,20 +12,8 @@ import "./assets/public/style.css"
 import '@/assets/font/HarmonyOS_Sans_SC/font.css'
 import '@/assets/font/UIcons/font.css'
 
-// 请求
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-// 国际化
-import i18n from './i18n'
-
-// 剪贴板
-import VueClipboard from 'vue-clipboard2'
-
-// 安装tooltip
-import tooltip from './components/tooltip/directive'
 
 // 安装toasts
 import toasts from './components/toasts'
 
-app.use(toasts).use(tooltip).use(VueClipboard).use(i18n).use(store).use(router).use(VueAxios, axios).mount('#app')
+app.use(toasts).mount('#app')
